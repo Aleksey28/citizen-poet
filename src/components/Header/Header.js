6e505048c15bb81a0ce14f0951858897db525e16
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-13 14:38:15 +0400

    feat: added base marckup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      Это шапка
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <main className="main">
+      Это тело
+    </main>
+  );
+}
+
+export default Main;
