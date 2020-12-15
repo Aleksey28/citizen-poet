3541484a35fa0e62de741424d1d274de9c531a72
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2020-12-15 15:27:07 +0300

    feat: Вторая страница - Разбил на блоки: кнопки вида, легенду и петиции - Добавил классы активности кнопок вида - Подготовил к резиновой верстке

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from "../NavBar/NavBar";
 import MainPage from "../Main/main-page/main-page";
+import PetitionPage from "../Main/petition-page/petition-page";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
-      <MainPage />
+      <PetitionPage />
     </div>
   );
 }
