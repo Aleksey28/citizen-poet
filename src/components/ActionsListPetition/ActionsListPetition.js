aa5278f8d7b561f68fad7da292fe6951cb8b4cf1
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2020-12-19 01:24:47 +0300

    feat: Сделать css action-list petition

--- src/components/ActionsListPetition/ActionsListPetition.js
+++ src/components/ActionsListPetition/ActionsListPetition.js
@@ -1,12 +1,14 @@
 import react from 'react';
 import './styles/ActionsListPetition.css'
-
+import BackButton from '../icons/ActiveList__back-btn'
 class ActionsListPetition extends react.Component {
   render() {
     return (
       <div className='action-list'>
-        <button type='button' className='action-list__button-back'></button>
-        <p className='action-list__text'></p>
+        <button type='button' className='action-list__button-back'>
+          <BackButton className='action-list__icon'/>
+        </button>
+        <p className='action-list__text'>Петиции</p>
         <input type='text' className='action-list__input-search' name='action-list__input-search' placeholder='Поиск' />
       </div>
     )
