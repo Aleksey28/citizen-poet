8244a38fdea98c9914fb1460dceb9682b657db85
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2020-12-18 23:13:02 +0300

    feat: Сделать легендуб прятать легенду при открытии петиции

--- src/components/PagePetition/PagePetition.js
+++ src/components/PagePetition/PagePetition.js
@@ -4,15 +4,17 @@ import ListPetition from '../ListPetition/ListPetition';
 import InfoPetition from '../InfoPetition/InfoPetition';
 import {useState} from "react";
 import SearchPetition from '../SearchPetition/SearchPetititon';
-
+import Legends from '../legend-petition/legends';
 const PagePetition = ({listPetition}) => {
 
   const [currentPetition, setCurrentPetition] = useState({});
   const [search, setShowSearch] = useState(true);
+  const [legends, setShowLegends] = useState(true);
 
   const handleClickOnPetition = (data) => {
     setCurrentPetition(data);
-    setShowSearch(false)
+    setShowSearch(false);
+    setShowLegends(false);
   }
 
 
@@ -24,6 +26,8 @@ const PagePetition = ({listPetition}) => {
         }
         <div className='page-petition__info'>
         <ListPetition listPetition={listPetition} onPetitionClick={handleClickOnPetition}/>
+        {legends && <Legends />}
+
         {Object.keys(currentPetition).length > 0 && <InfoPetition currentPetition={currentPetition}/>}
         </div>
       </div>
---
+++ src/components/icons/legends/DarkBlueCircle.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+function DarkBlueCircle(props) {
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 94 94"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle cx={47} cy={47} r={47} fill="#1A263D" />
+    </svg>
+  )
+}
+
+export default DarkBlueCircle;
