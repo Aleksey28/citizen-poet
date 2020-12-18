c12e5c534d5e9d2e9cbcf142d1de11dfaeae57fa
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2020-12-19 00:51:18 +0300

    feat: Сделать добовление блока Назад

--- src/components/ListPetition/ListPetition.js
+++ src/components/ListPetition/ListPetition.js
@@ -1,14 +1,22 @@
 import './ListPetition.css';
 import React from "react";
+import {useState} from "react";
 import ItemPetition from '../ItemPetition/ItemPetition';
 
 
 const ListPetition = ({listPetition, onPetitionClick}) => {
+
+  const [backBlock, setBackBlock] = useState(false);
+
+  const onBackBlock = () => {
+    setBackBlock(true);
+  }
   return (
       <div className="list-petition">
+        {(backBlock && <h2>back</h2>)}
       {
         listPetition.map(item => {
-          return <ItemPetition item={item} onPetitionClick={onPetitionClick}/>
+          return <ItemPetition item={item} onPetitionClick={onPetitionClick} onBackBlock={onBackBlock} />
         })
       }
     </div>
