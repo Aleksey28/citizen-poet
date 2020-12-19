2782acab135a56cf74dcf8ec0a5bc7c913c33c52
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2020-12-19 21:15:14 +0300

    feat: Сделать отоброжения рейтинга и петиций пользователя, добавить кнопку выходы из профиля

--- src/components/InfoProfile/InfoProfile.js
+++ src/components/InfoProfile/InfoProfile.js
@@ -1,11 +1,26 @@
 import './styles/InfoProfile.css';
+import DefaultIcon from '../icons/profile/DefaultIcon';
 
 const InfoProfile = ({user}) => {
 
+  // функция отоброджения аватарки, если null значит дефолтная иконка, если есть дата или url, грузил по ним
+  const profileIcon = function(logo) {
+    if (logo == null) {
+      return <DefaultIcon className="info-profile__avatar"/> 
+    } else {
+      return <img src="http://i1.ytimg.com/vi/XKFSikgM0yY/maxresdefault.jpg" alt="Аватар пользователя" className="info-profile__avatar"/>
+    };
+  }
+
   return (
     <div className="info-profile">
-      <img src="http://i1.ytimg.com/vi/XKFSikgM0yY/maxresdefault.jpg" alt="Аватар пользователя" className="info-profile__avatar"/>
-      <h2 className="info-profile__name">Пушкин АСи</h2>
+      {
+        profileIcon(user.logo)
+      }
+      <h2 className="info-profile__name">{user.fullName()}</h2>
+      <p className='info-profile__text'>Ваш рейтинг: {user.reiting}</p>
+      <p className='info-profile__text'>Создано петиций: {user.petitonCount}</p>
+      <button type='button' className='info-profile__log-out'>Выйти</button>
     </div>
   )
 }
--- src/data/user__prof.js
+++ src/data/user__prof.js
@@ -1,20 +1,23 @@
+import userPet from '../data/user__pet';
+
 class UserData {
-  constructor(name, soname, lastName, age, logo, reiting) {
+  constructor(name, soname, lastName, age, logo, reiting, petitonCount) {
     this.name = name;
     this.soname = soname;
     this.lastName = lastName;
     this.age = age;
     this.logo = logo;
     this.reiting = reiting;
+    this.petitonCount = petitonCount;
   }
 
   fullName() {
-    return `${this.soname} ${this.name} ${this.lastName}`;
+    return (this.soname + ' ' + this.name + ' ' + this.lastName) ;
   }
 
 }
 
 
 
-const user = new UserData('Ivan', 'Ivanov', 'Ivanich', '14.04.1999', null, '4');
+const user = new UserData('Ivan', 'Ivanov', 'Ivanich', '14.04.1999', null, '4', userPet.length);
 export default user;
